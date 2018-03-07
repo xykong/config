78e2b256f0e2d8397da9d3593583aa57c0c3cef4
Zeal <xujinzheng@gmail.com>
2018-03-07 12:53:20 +0800

    fix panic issue

--- config_hocon.go
+++ config_hocon.go
@@ -1,6 +1,9 @@
 package config
 
 import (
+	"math/big"
+	"time"
+
 	"github.com/go-akka/configuration"
 )
 
@@ -17,7 +20,7 @@ func NewHOCONConfiguration(conf *configuration.Config) Configuration {
 func (p *HOCONConfiguration) GetConfig(path string) Configuration {
 	conf := p.Config.GetConfig(path)
 	if conf == nil {
-		return nil
+		return (*HOCONConfiguration)(nil)
 	}
 	return &HOCONConfiguration{conf}
 }
@@ -31,8 +34,158 @@ func (p *HOCONConfiguration) WithFallback(fallback Configuration) Configuration 
 	return p
 }
 
+func (p *HOCONConfiguration) GetBoolean(path string, defaultVal ...bool) bool {
+	if p == nil || p.Config == nil {
+		if len(defaultVal) > 0 {
+			return defaultVal[0]
+		}
+		return false
+	}
+	return p.Config.GetBoolean(path, defaultVal...)
+}
+
+func (p *HOCONConfiguration) GetByteSize(path string) *big.Int {
+	if p == nil || p.Config == nil {
+		return nil
+	}
+	return p.Config.GetByteSize(path)
+}
+
+func (p *HOCONConfiguration) GetInt32(path string, defaultVal ...int32) int32 {
+	if p == nil || p.Config == nil {
+		if len(defaultVal) > 0 {
+			return defaultVal[0]
+		}
+		return 0
+	}
+	return p.Config.GetInt32(path, defaultVal...)
+}
+
+func (p *HOCONConfiguration) GetInt64(path string, defaultVal ...int64) int64 {
+	if p == nil || p.Config == nil {
+		if len(defaultVal) > 0 {
+			return defaultVal[0]
+		}
+		return 0
+	}
+	return p.Config.GetInt64(path, defaultVal...)
+}
+
+func (p *HOCONConfiguration) GetString(path string, defaultVal ...string) string {
+	if p == nil || p.Config == nil {
+		if len(defaultVal) > 0 {
+			return defaultVal[0]
+		}
+		return ""
+	}
+	return p.Config.GetString(path, defaultVal...)
+}
+
+func (p *HOCONConfiguration) GetFloat32(path string, defaultVal ...float32) float32 {
+	if p == nil || p.Config == nil {
+		if len(defaultVal) > 0 {
+			return defaultVal[0]
+		}
+		return 0
+	}
+	return p.Config.GetFloat32(path, defaultVal...)
+}
+
+func (p *HOCONConfiguration) GetFloat64(path string, defaultVal ...float64) float64 {
+	if p == nil || p.Config == nil {
+		if len(defaultVal) > 0 {
+			return defaultVal[0]
+		}
+		return 0
+	}
+	return p.Config.GetFloat64(path, defaultVal...)
+}
+
+func (p *HOCONConfiguration) GetTimeDuration(path string, defaultVal ...time.Duration) time.Duration {
+	if p == nil || p.Config == nil {
+		if len(defaultVal) > 0 {
+			return defaultVal[0]
+		}
+		return 0
+	}
+	return p.Config.GetTimeDuration(path, defaultVal...)
+}
+
+func (p *HOCONConfiguration) GetTimeDurationInfiniteNotAllowed(path string, defaultVal ...time.Duration) time.Duration {
+	if p == nil || p.Config == nil {
+		if len(defaultVal) > 0 {
+			return defaultVal[0]
+		}
+		return 0
+	}
+	return p.Config.GetTimeDurationInfiniteNotAllowed(path, defaultVal...)
+}
+
+func (p *HOCONConfiguration) GetBooleanList(path string) []bool {
+	if p == nil || p.Config == nil {
+		return nil
+	}
+	return p.Config.GetBooleanList(path)
+}
+
+func (p *HOCONConfiguration) GetFloat32List(path string) []float32 {
+	if p == nil || p.Config == nil {
+		return nil
+	}
+	return p.Config.GetFloat32List(path)
+}
+
+func (p *HOCONConfiguration) GetFloat64List(path string) []float64 {
+	if p == nil || p.Config == nil {
+		return nil
+	}
+	return p.Config.GetFloat64List(path)
+}
+
+func (p *HOCONConfiguration) GetInt32List(path string) []int32 {
+	if p == nil || p.Config == nil {
+		return nil
+	}
+	return p.Config.GetInt32List(path)
+}
+
+func (p *HOCONConfiguration) GetInt64List(path string) []int64 {
+	if p == nil || p.Config == nil {
+		return nil
+	}
+	return p.Config.GetInt64List(path)
+}
+
+func (p *HOCONConfiguration) GetByteList(path string) []byte {
+	if p == nil || p.Config == nil {
+		return nil
+	}
+	return p.Config.GetByteList(path)
+}
+
+func (p *HOCONConfiguration) GetStringList(path string) []string {
+	if p == nil || p.Config == nil {
+		return nil
+	}
+	return p.Config.GetStringList(path)
+}
+
+func (p *HOCONConfiguration) HasPath(path string) bool {
+	if p == nil || p.Config == nil {
+		return false
+	}
+	return p.Config.HasPath(path)
+}
+
 func (p *HOCONConfiguration) Keys() []string {
+	if p == nil || p.Config == nil {
+		return nil
+	}
 	return p.Config.Root().GetObject().GetKeys()
+}
+
+func (p *HOCONConfiguration) IsEmpty() bool {
+	return p == nil || p.Config.IsEmpty()
 }
 
 type HOCONConfigProvider struct {
